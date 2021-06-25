22bb1690c3ff4e9ef7dc58067818cff3524f130a
Pratyusha Routh <pratyusharouth2001@gmail.com>
2021-06-25 16:16:52 +0530

    ✨ Table Heading

---
+++ src/components/DataHeading.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Container } from 'react-bootstrap'
+import './Data.css'
+
+const DataHeading=()=>{
+    return(
+       <Container className="data-heading">
+           LATEST AUCTION
+       </Container>
+    )
+}
+
+export default DataHeading;
